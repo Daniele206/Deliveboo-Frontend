9b02dd831e745c3bd6c99b11273d08c9c7b7d181
Daniele206 <danieledeieso2004@gmail.com>
2024-06-19 15:09:21 +0200

    [fix] responsive layout header & type selector

--- src/partials/Header.vue
+++ src/partials/Header.vue
@@ -5,13 +5,14 @@ export default {
 </script>
 
 <template>
-  <div>
+  <div class="fixer">
     <header class="container">
       <div class="row align-content-center header_d">
-        <router-link class="col logo p-0" :to="{name: 'home'}">
+        <router-link class="col col-5 logo p-0 d-flex text-decoration-none" :to="{name: 'home'}">
           <img src="/src/assets/img/logo.jpg" alt="Logo" class="logo_img"/>
+          <h1 class="text_my_red fw-bold ps-2 lh_header">DeliverBoo</h1>
         </router-link>
-        <nav class="nav col d-flex justify-content-end p-0">
+        <nav class="nav col col-7 d-flex justify-content-end p-0">
           <router-link class="my_btn text-decoration-none" :to="{name: 'restaurants'}">Ristoranti</router-link>
           <router-link class="my_btn text-decoration-none" :to="{name: 'cart'}"><i class="fa-solid fa-cart-shopping"></i></router-link>
         </nav>
@@ -22,15 +23,39 @@ export default {
 
 <style scoped lang="scss">
 @use "../assets/scss/variables" as *;
+
+.fixer{
+  position: fixed;
+  width: 100%;
+  z-index: 999;
+}
+
 .header_d {
   margin-top: 20px;
   padding: 10px 30px;
   background-color: white;
+  border: 1px solid $primary_red;
   color: black;
   border-radius: 50px;
 }
 
 .logo {
   height: 50px;
+}
+
+.lh_header{
+  height: 50px;
+  line-height: 50px;
+  font-size: 2rem;
+}
+
+@media screen and (max-width: 576px) {
+  .header_d{
+    border-radius: 0;
+    margin-top: 0;
+  }
+  .lh_header{
+    font-size: 1.3rem;
+  }
 }
 </style>
