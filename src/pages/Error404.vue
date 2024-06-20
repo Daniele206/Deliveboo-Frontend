35761f3632c0143f007ac08ff1b2209804f8ee8f
giorgia-rondinelli <g.rondinelli4@gmail.com>
2024-06-20 12:34:42 +0200

    [add] page error 404 e loader

--- src/pages/Error404.vue
+++ src/pages/Error404.vue
@@ -1,7 +1,17 @@
 <template>
-  <div>
-    <h1>Error 404</h1>
-    <p>Pagina Non Trovata</p>
+  <div class="container text-center containerError align-content-center">
+    <!-- <div class="imgContainer">
+      <img src="/src/assets/img/sad-folder.png" alt="">
+    </div> -->
+    <!-- <div class="imgContainer">
+      <img src="/src/assets/img/girl-icecream.png" alt="">
+    </div> -->
+    <div class="imgContainer">
+      <img src="/src/assets/img/sad-icecream.png" alt="">
+    </div>
+    <p class="error">Error 404 <i class="fa-solid fa-triangle-exclamation"></i></p>
+   
+    <p class="notFound">Pagina Non Trovata</p>
   </div>
 </template>
 
@@ -12,5 +22,30 @@
 </script>
 
 <style lang="scss" scoped>
+.imgContainer{
+  height: 200px;
+ 
+  // img{
+  //   -webkit-filter: invert(100%);
+  //   filter: invert(100%);
+  // }
+}
+.containerError{
+height: 400px;
+line-height: 35px;
+color: white;
+.sad{
+  font-size: 4rem;
+}
+.error{
+  font-size: 3.5rem;
+  font-weight: 600;
+
+}
+.notFound{
+  font-size: 2.5rem;
+}
+
+}
 
 </style>
