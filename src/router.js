f7adc980d3d3d3a1fb63d4dfd9e4cbf3baca6094
AlessiaCiabatti <alessiaciaba@gmail.com>
2024-06-21 15:01:22 +0200

    [add] scrollbar

--- src/router.js
+++ src/router.js
@@ -5,30 +5,36 @@ import Restaurants from './pages/Restaurants.vue';
 import Cart from './pages/Cart.vue';
 import Error404 from './pages/Error404.vue';
 
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/restaurants',
+    name: 'restaurants',
+    component: Restaurants
+  },
+  {
+    path: '/cart',
+    name: 'cart',
+    component: Cart
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'error-404',
+    component: Error404
+  }
+];
+
+// Configura il router con la funzione scrollBehavior
 const router = createRouter({
   history: createWebHashHistory(),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/restaurants',
-      name: 'restaurants',
-      component: Restaurants
-    },
-    {
-      path: '/cart',
-      name: 'cart',
-      component: Cart
-    },
-    {
-      path: '/:pathMatch(.*)*',
-      name: 'error-404',
-      component: Error404
-    }
-  ]
-})
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    return { top: 0 };
+  }
+});
 
-export { router }
+export { router };
