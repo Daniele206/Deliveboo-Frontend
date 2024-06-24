2e6c0a3a5f14203d81c20407fe28a037a85e5023
Daniele206 <danieledeieso2004@gmail.com>
2024-06-24 14:43:13 +0200

    [add] count

--- src/components/Dish.vue
+++ src/components/Dish.vue
@@ -7,14 +7,17 @@
 
     data(){
       return{
-        arrayData: []
+        arrayData: [],
+        nItem: 0
       }
     },
 
     methods:{
-      setData(dish){
-        if(!localStorage.getItem('cart')){
-          localStorage.setItem('cart', JSON.stringify([dish]))
+      
+      addItem(dish){
+        if(!localStorage.getItem('cart') || localStorage.getItem('cart') === JSON.stringify([])){
+          this.arrayData = [dish];
+          localStorage.setItem('cart', JSON.stringify(this.arrayData));
           console.log(localStorage.getItem('cart'));
         }else{
           this.arrayData = JSON.parse(localStorage.getItem('cart'));
@@ -27,6 +30,42 @@
             return alert('Non puoi aggiungere un piatto di un altro ristorante')
           }
         }
+      },
+
+      removeItem(dish){
+        if(!localStorage.getItem('cart') || localStorage.getItem('cart') === JSON.stringify([])){
+          return alert('Il carrello é vuoto');
+        }else{
+          this.arrayData = JSON.parse(localStorage.getItem('cart'));
+          if(this.arrayData.some(item => JSON.stringify(item) === JSON.stringify(dish))){
+            for (let i = 0; i < this.arrayData.length; i++) {
+              if (JSON.stringify(this.arrayData[i]) === JSON.stringify(dish)) {
+                this.arrayData.splice(i, 1);
+                    break;
+                }
+            }
+            localStorage.setItem('cart', JSON.stringify(this.arrayData));
+            console.log(localStorage.getItem('cart'));
+          }else{
+            return alert('Questo piatto non é nel carrello')
+          }
+        }
+      },
+
+      count(){
+        this.nItem = 0;
+        this.arrayData = JSON.parse(localStorage.getItem('cart'));
+        for (let i = 0; i < this.arrayData.length; i++) {
+                if (JSON.stringify(this.arrayData[i]) === JSON.stringify(this.dish)) {
+                  this.nItem++
+                }
+            }
+      },
+    },
+
+    mounted(){
+      if(localStorage.getItem('cart')){
+        this.count()
       }
     }
   }
@@ -36,12 +75,15 @@
   <div v-if="dish.is_visible == 1" class="card mx-3 my-4" style="width: 18rem;">
   <div class="card-body">
     <h2 class="card_title">{{ dish.name }}</h2>
-    <h2 class="card_title">{{ dish.restaurant_id }}</h2>
     <img class="card_img" :src="dish.image" :alt="dish.name">
     <p class="card_description">{{ dish.description }}</p>
     <div class="d-flex align-items-end justify-content-between">
       <span>&euro;{{ dish.price }}</span>
-      <button @click="setData(dish)" class="my_btn"><i class="fa-solid fa-cart-plus"></i></button>
+      <div class="d-flex">
+        <button v-if="nItem > 0" @click="removeItem(dish), count()" class="my_btn me-2"><i class="fa-solid fa-trash-can"></i></button>
+        <button v-if="nItem === 0" @click="addItem(dish), count()" class="my_btn"><i class="fa-solid fa-cart-plus"></i></button>
+        <button v-else @click="addItem(dish), count()" class="my_btn">{{ nItem }} +</button>
+      </div>
     </div>
   </div>
 </div>
