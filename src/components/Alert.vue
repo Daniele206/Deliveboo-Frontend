b6ce37e85ebf26994c730dd3374baf3da99e57ac
Daniele206 <danieledeieso2004@gmail.com>
2024-06-25 14:56:08 +0200

    [add] alert mod

---
+++ src/components/Alert.vue
@@ -0,0 +1,59 @@
+<script>
+
+   // store
+  import { store } from '../data/store';
+  // store
+
+  export default {
+    data(){
+      return{
+        store,
+        restaurant: {},
+        restaurantName: '',
+        cart: JSON.parse(localStorage.getItem('cart')),
+      }
+    },
+
+    methods:{
+
+      deleteCart(){
+        localStorage.removeItem('cart');
+        localStorage.removeItem('dishQuantity');
+      },
+
+      redirectMenu(){
+        this.store.restaurants.forEach(restaurant => {
+          if(restaurant.id === this.cart[0].restaurant_id){
+            this.restaurant = restaurant
+          }
+        });
+        localStorage.setItem('dishes', JSON.stringify(this.restaurant.dishes))
+        localStorage.setItem('restaurantName', this.restaurant.name)
+      }
+    }
+  }
+</script>
+
+<template>
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Ordine Univoco</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <p>Non puoi ordinare da piú risotranti contemporaneamente!</p>
+        </div>
+        <div class="modal-footer">
+          <button @click="deleteCart" type="button" class="btn btn-secondary" data-bs-dismiss="modal">Svuota Carrello</button>
+          <button @click="redirectMenu" type="button" class="btn btn-primary" data-bs-dismiss="modal">Ritorna Menu Ristorante Ordine</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,7 @@
 <script>
 import Hero from "../partials/Hero.vue"
 import Swiper from "../components/Swiper.vue";
+import Alert from "../components/Alert.vue";
 import Consegna from "../partials/Consegna.vue";
 import Scarica from "../partials/Scarica.vue";
 
@@ -18,6 +19,7 @@ import Scarica from "../partials/Scarica.vue";
       Swiper,
       Consegna,
       Scarica,
+      Alert,
     },
 
     data(){
@@ -54,6 +56,11 @@ import Scarica from "../partials/Scarica.vue";
   <Swiper 
   :types="types"
   />
+
+  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Launch demo modal
+  </button>
+  <Alert />
 
   <Consegna />
 
